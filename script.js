7cdb074bff3176a213c9a999f9482ca5e1f81a1d
CollinDG <154558368+CollinDG@users.noreply.github.com>
2025-04-15 15:18:46 -0700

    Add functionality to highlight latest operator

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ const display = document.querySelector(".display");
 const numButtons = document.querySelectorAll("button.num")
 numButtons.forEach((button) => {
     button.addEventListener("click", () => {
+        // Add pressed number to display
         const num = button.innerText;
         if (display.innerText === "0") {
             display.innerText = num;
@@ -35,22 +36,39 @@ numButtons.forEach((button) => {
 let operator;
 let numbers = [0];
 
-const equals = document.querySelector("button.equals");
-equals.addEventListener("click", () => {
-    wip
-})
+// const equals = document.querySelector("button.equals");
+// equals.addEventListener("click", () => {
+//     
+// })
 
 const opButtons = document.querySelectorAll("button.operand");
 opButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        wip
+        // Add display number to numbers array
+        const displayNum = display.innerText;
+        if (numbers[0] === 0 && numbers.length === 1) {
+            numbers[0] = displayNum;
+        } else {
+            numbers.push(displayNum);
+        }
+
+        // Highlight latest clicked operator
+        operator = button.innerText;
+        const highlightedButton = document.querySelector("button.highlighted");
+        if (!highlightedButton) {
+            button.classList.add("highlighted");
+        }
+        else if (operator !== highlightedButton.innerText) {
+            highlightedButton.classList.remove("highlighted");
+            button.classList.add("highlighted");
+        }
     })
 })
 
 ```
 [2] [4] [+] [-] [3] [5] [*] [2] [=]
-when each number is pressed, add it to display
-when operand is pressed, add display number to numbers array (replace 0)
+- when each number is pressed, add it to display
+- when operand is pressed, add display number to numbers array (replace 0)
 highlight latest operand until a number is pressed, then store the operand
 when another operand is pressed, add display number to numbers array
 because there are now 2 numbers in array, operate on them and display it
